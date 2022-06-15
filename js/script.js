4327daef898272052d7132dd3c794f502a93140b
Bhanu Singh <59247235+biasedzeus@users.noreply.github.com>
2022-06-15 19:31:54 +0530

    hmbrgr-btn toggle

---
+++ js/script.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded",() =>{
+    initApp();
+})
+
+const initApp = () =>{
+    const menuBtn = document.querySelector(".nav__burger-btn")
+    const list = document.querySelector(".nav__list");
+    menuBtn.addEventListener("click",toggleButtonAndMenu)
+    // list.addEventListener("click",toggleButtonAndMenu)
+
+}
+
+const toggleButtonAndMenu = () =>{
+    const menuBtn = document.querySelector(".nav__burger-btn")
+    const list = document.querySelector(".nav__list");
+    menuBtn.classList.toggle("open");
+    // list.classList.toggle("open");
+
+    
+}
